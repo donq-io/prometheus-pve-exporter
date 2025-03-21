e0873c9752b8649bcccf78c75f12d3777a132635
Federico Serafini <federico@donq.io>
2025-03-21 09:11:54 +0100

    Aggiungi configurazione per Devenv e direnv
    - Crea il file .envrc per la configurazione di direnv.
    - Aggiungi .devenv* e .direnv al .gitignore.
    - Crea il file devenv.lock per gestire le dipendenze di Devenv.
    - Aggiungi il file devenv.nix per la configurazione dell'ambiente di sviluppo.
    - Crea il file devenv.yaml per la definizione degli input di Devenv.

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = ./requirements.in;
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+}
